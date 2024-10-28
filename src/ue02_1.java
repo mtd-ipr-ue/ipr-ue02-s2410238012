0bbed71b16c7473314be56b3772a8bc363d77026
s2410238012 <s2410238012@fhooe.at>
2024-10-28 17:02:30 +0100

    ue2 first commit

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,27 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.println("First Number:");
+        int numberOne = In.readInt();
+
+        Out.println("Second Number:");
+        int numberTwo = In.readInt();
+
+        Out.println("Third Number:");
+        int numberThree = In.readInt();
+
+        int max = 0;
+
+        if(numberOne >= numberTwo && numberOne >= numberThree) {
+            max = numberOne;
+        } else if (numberTwo > numberOne && numberTwo >= numberThree){
+            max = numberTwo;
+        } else {
+            max = numberThree;
+        }
+        Out.println("maximum:");
+        Out.println(max);
     }
 }
